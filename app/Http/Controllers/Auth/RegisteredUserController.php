ae231fbdd19b37871a0ac1ea616491922874498c
Patryk Pilek <patryk.pilek@gmail.com>
2023-02-19 02:54:44 +0000

    automatically bind the challenge to a button

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -5,11 +5,13 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use App\Providers\RouteServiceProvider;
+use Closure;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Validation\Rules;
 use Illuminate\View\View;
 
@@ -34,6 +36,17 @@ class RegisteredUserController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:'.User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            'g-recaptcha-response' => ['required', function(string $attribute, mixed $value, Closure $fail) {
+                $g_response = Http::asForm()->post("https://www.google.com/recaptcha/api/siteverify", [
+                    'secret' => config('services.recaptcha.secret_key'),
+                    'response' => $value,
+                    'remoteip' => \request()->ip(),
+                ]);
+
+                if(!$g_response->json('success')) {
+                    $fail("The {$attribute} is invalid.");
+                }
+            },]
         ]);
 
         $user = User::create([
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,5 +1,5 @@
 <x-guest-layout>
-    <form method="POST" action="{{ route('register') }}">
+    <form method="POST" action="{{ route('register') }}" id="registerForm">
         @csrf
 
         <!-- Name -->
@@ -44,9 +44,19 @@
                 {{ __('Already registered?') }}
             </a>
 
-            <x-primary-button class="ml-4">
+            <x-primary-button class="g-recaptcha ml-4"
+                              data-sitekey="{{ config('services.recaptcha.site_key') }}"
+                              data-callback='onSubmit'
+                              data-action='register'>
                 {{ __('Register') }}
             </x-primary-button>
         </div>
     </form>
+    @push('scripts')
+        <script>
+            function onSubmit(token) {
+                document.getElementById("registerForm").submit();
+            }
+        </script>
+    @endpush
 </x-guest-layout>
